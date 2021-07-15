05057cc24c5408badab6c634c71d8a9c8c4ab9b6
Andres Andrade <andres.63.d@gmail.com>
2021-07-15 11:51:46 -0300

    [UPD][sale_viatic] Incidence nbr./s.: ODOO-1527.  - Required field, cost price

--- sale_viatic/__manifest__.py
+++ sale_viatic/__manifest__.py
@@ -11,7 +11,7 @@
     'website': "http://www.calyxservicios.com.ar",
     'category': 'Sales',
     'version': '11.0.1.0.0',
-    'depends' : ['sale', 'sale_margin', 'l10n_ar_sale'],
+    'depends' : ['sale', 'sale_margin', 'l10n_ar_sale', 'sale_margin'],
 
     'data': [
         'security/security.xml',
