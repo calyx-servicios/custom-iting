5ea9b3177193f042d265197d646001da7f8cc41e
Esteban Suarez <estebansamsuarez20@gmail.com>
2025-07-13 20:17:05 -0300

    [UPD][l10n_ar_afip_ws_ARCA, l10n_ar_arca_transparencia_fiscal]

--- l10n_ar_arca_transparencia_fiscal/__manifest__.py
+++ l10n_ar_arca_transparencia_fiscal/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_ar_afip_ws_ARCA",
+    'name': "l10n_ar_arca_transparencia_fiscal",
     'summary': """
-        This module customizes the lot number labels.
     """,
     'author': "Calyx Servicios S.A",
     "maintainers": ["estebansam21", "enzogonzalezdev"],
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Account",
-    "version": "11.0.3.0.1",
+    "version": "11.0.1.0.0",
     "application": False,
     'depends': [
-                'l10n_ar_afipws',
-                'l10n_ar_afipws_fe'
+            'account'
                 ],
     'data': [
         # 'views/res_config_settings.xml',
-        'views/view_move_form.xml',
+        'views/report_invoice.xml',
+        'data/account_tax_group_data.xml'
     ],
 }
